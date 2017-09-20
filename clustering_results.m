313b14183d47827e303c2c94fd503ee5a881956c
Venkatraman Renganathan <vrengana@utdallas.edu>
2017-09-19 21:23:11 -0500

    Cluster Pruning Distance code added

--- clustering_results.m
+++ clustering_results.m
@@ -1,4 +1,4 @@
-function [ similar_cluster ] = clustering_results( agents_signal_fingerprints )
+function [ similar_cluster ] = clustering_results( m, agents_signal_fingerprints )
 % clustering_results takes signal fingerprints of all agents as input and
 % returns the clusters that have more than 1 elements depicting that the
 % fingerprints grouped in same clusters are similar and are spoofed.
@@ -12,14 +12,34 @@ function [ similar_cluster ] = clustering_results( agents_signal_fingerprints )
 % similar_cluster - array having agents indices that have similar
 % fingerprints.
 %
-    
-    k = 7;
-    similar_cluster = kmeans(agents_signal_fingerprints,k,'Distance','correlation');
+%     similar_cluster = [];
+    agents_signal_fingerprints = [];
+    angle_of_arrival = [240 160 260 90 290 20 330 325]';
+    reflected_arrival_angles = [210 140 245 115 300 35 320 330]';
+    first_amplitude = [10 40 50 25 65 75 55 55]';
+    second_aplitude = 0.5*first_amplitude;
+    a = 50;
+    b = 100;
+    arrival_time = (b-a).*rand(m,1) + a;
+    agents_signal_fingerprints = [angle_of_arrival reflected_arrival_angles first_amplitude second_aplitude arrival_time];
     Y = pdist(agents_signal_fingerprints);
     Z = linkage(Y)
-    c = cophenet(Z,Y)
-    dendrogram(Z)
-    
-    
+    for i = 1:size(Z,1)
+        if(max(Z(i,1:2)) > m)
+            cutoff_distance = Z(i,3);
+            break;
+        end
+    end
+    % Lower the cutoff_distance value by some epsilon for pruning
+    cutoff_distance = cutoff_distance - 0.01 
+    I = inconsistent(Z);
+    T = cluster(Z,'cutoff',cutoff_distance,'criterion','distance')
+    u = unique(T);
+    n = histc(T,u);
+    find(ismember(T,u(n>1)))
+    %T = cluster(Z,'maxclust',m-1)
+    figure;
+    dendrogram(Z,'ColorThreshold','default')
+    print 'hi'
 end
 
